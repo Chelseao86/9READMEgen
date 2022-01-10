c6f6cf3f0a2bec8a0319dbd9481918eb2f8e6d20
Chelseao86 <92495487+Chelseao86@users.noreply.github.com>
2022-01-10 16:30:57 -0700

    after tutor 220110

---
+++ Develop/README.md
@@ -0,0 +1,13 @@
+
+   # title
+   ## Description
+   desc
+   ## Table of Contents:
+   + [Description](#description)
+   + [License](#license)
+   ## License
+   [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+  
+   ## Question: If you have any question, feel free to contact me at chelseaholmstead@gmail.com.
+
+   
--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,63 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs')
 
 console.log(inquirer)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+  type:"input",
+  name:"title",
+  message:"What is your project title?",
+},
+{
+  type:"input",
+  name:"description",
+  message:"What is your project desription?",
+},
+{
+  type:"list",
+  name:"license",
+  message:"What license is the project under?",
+  choices:["MIT","IST"],
+},
+{
+  type:"input",
+  name:"email",
+  message:"What is your email?",
+}
+];
 
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    JSON.stringify(answers)
+    console.log(JSON.stringify(answers))
+   fs.writeFileSync("README.md", `
+   # ${answers.title}
+   ## Description
+   ${answers.description}
+   ## Table of Contents:
+   + [Description](#description)
+   + [License](#license)
+   ## License
+   [![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)](https://opensource.org/licenses/${answers.license})
+  
+   ## Question: If you have any question, feel free to contact me at ${answers.email}.
+
+   `
+       
+       )
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
