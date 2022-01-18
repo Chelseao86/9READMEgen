601d3308050725bb6e3824039ffb753f74aabc8e
Chelseao86 <92495487+Chelseao86@users.noreply.github.com>
2022-01-18 16:31:40 -0700

    push edits

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,16 @@
 function generateMarkdown(answers) {
    return `
    # ${answers.title}
-   ## License
    [![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)](https://opensource.org/licenses/${answers.license})
    ## Description
    ${answers.description}
    ## Table of Contents:
-   + [License](#license)
-   + [Description](#description)
    + [Installation](#installation)
    + [Usage](#usage)
    + [Contribution](#contribution)
    + [Testing](#testing)  
   
-  
    ## Questions: If you have any questions, feel free to contact ${answers.firstName} at ${answers.email}.
-
    `
 }
 module.exports=generateMarkdown
